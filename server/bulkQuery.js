5c7ac6c66b0469a2b57545a25d42f732c7b7af75
FunnyBear <65567444+funnybear0718@users.noreply.github.com>
2020-06-25 18:35:40 +0300

    bulkQueryUpdate-1

--- server/bulkQuery.js
+++ server/bulkQuery.js
@@ -1,3 +1,6 @@
+const util = require('util');
+const request = require('request');
+
 const bulkQuery = async(ctx, accessToken, shop) => {
     const query = JSON.stringify({
         query: `
@@ -8,6 +11,7 @@ const bulkQuery = async(ctx, accessToken, shop) => {
               orders{
                 edges {
                   node {
+                    id
                     name
                     customer {
                       displayName
@@ -17,6 +21,13 @@ const bulkQuery = async(ctx, accessToken, shop) => {
                         zip
                       }
                     }
+                    originalTotalPriceSet {
+                      shopMoney {
+                        amount
+                        currencyCode
+                      }
+                    }
+                    tags
                   }
                 }
               }
@@ -49,42 +60,72 @@ const bulkQuery = async(ctx, accessToken, shop) => {
 
     const responseJson = await response.json();
 
-    console.log(responseJson.data.bulkOperationRunQuery.bulkOperation);
+    // console.log(responseJson.data.bulkOperationRunQuery.bulkOperation.id);
 
-    const query1 = JSON.stringify({
-        query: `
-        query currentBulkOperation{
-          currentBulkOperation {
-            id
-            status
-            errorCode
-            createdAt
-            completedAt
-            objectCount
-            fileSize
-            url
-            partialDataUrl
+    const bulkOperationId = responseJson.data.bulkOperationRunQuery.bulkOperation.id;
+
+    var bulkOperationStatus = 'RUNNING';
+
+    var responseJson1 = responseJson;
+
+    while (bulkOperationStatus == 'RUNNING') {
+
+        let query1 = JSON.stringify({
+            query: `
+          query{
+            node(id: "` + bulkOperationId + `") {
+              ... on BulkOperation {
+                id
+                status
+                errorCode
+                createdAt
+                completedAt
+                objectCount
+                fileSize
+                url
+                partialDataUrl
+              }
+            }
           }
-        }
-      `
-    });
+        `
+        });
 
-    const response1 = await fetch(
-        `https://${shop}/admin/api/2020-04/graphql.json`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "X-Shopify-Access-Token": accessToken
-            },
-            body: query1
-        }
-    );
+        let response1 = await fetch(
+            `https://${shop}/admin/api/2020-04/graphql.json`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-Shopify-Access-Token": accessToken
+                },
+                body: query1
+            }
+        );
 
-    const responseJson1 = await response1.json();
+        responseJson1 = await response1.json();
 
-    console.log(responseJson1);
+        bulkOperationStatus = responseJson1.data.node.status;
+    }
+    // console.log(responseJson1);
 
-    // return ctx.redirect(confirmationUrl);
+    const fileUrl = responseJson1.data.node.url;
+    var result = '';
+
+    const requestPromise = util.promisify(request);
+    const response2 = await requestPromise(fileUrl);
+    // request.get(fileUrl, function(error, response, body) {
+    //     result = body;
+    // });
+    // let response2 = await fetch(fileUrl, {
+    //     method: "GET"
+    // });
+    // console.log(response2);
+    result = response2.body;
+
+    return result;
+    // return JSON.stringify(result);
+
+    // return JSON.stringify(Array());
+    // return ctx.redirect(confirmationUrl.data.node.url);
 };
 
 module.exports = bulkQuery;
